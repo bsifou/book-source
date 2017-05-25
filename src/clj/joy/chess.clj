deaf5fbce8a2e2d3abf6eeb03fc7763959c68469
Sifu Biri <sifeddinebiri@gmail.com>
2017-05-25 15:51:02 -0500

    different encapsulations

--- src/clj/joy/chess.clj
+++ src/clj/joy/chess.clj
@@ -1,7 +1,7 @@
 (ns joy.chess)
 
 (defn initial-board []
-  [\r \n \b \q \k \b \n \r       
+  [\r \n \b \q \k \b \n \r
    \p \p \p \p \p \p \p \p
    \- \- \- \- \- \- \- \-
    \- \- \- \- \- \- \- \-
@@ -10,9 +10,12 @@
    \P \P \P \P \P \P \P \P
    \R \N \B \Q \K \B \N \R])
 
+
+
 (def ^:dynamic *file-key* \a)
 (def ^:dynamic *rank-key* \0)
 
+;; namespace encapsulation
 (defn- file-component [file]
   (- (int file) (int *file-key*)))
 
@@ -29,5 +32,36 @@
   (let [[file rank] pos]
     (board (index file rank))))
 
+;; closure encapsulation
+(letfn [(index [[file rank]]
+          (let [f (- (int file) (int \a))
+                r (* 8 (- 8 (- (int rank) (int \0))))]
+            (+ f r)))]
+  (defn lookup2 [board pos]
+    (board (index pos))))
+
+
+
+;; local encapsulation (most specific scope)
+
+(defn lookup3 [board [file rank]]
+  (let [f (- (int file) (int \a))
+        r (* 8 (- 8 (- (int rank) (int \0))))
+        index (+ f r)]
+    (board index)))
+
+
+
 ;; (lookup (initial-board) "a8")
 ;;=> \r
+
+
+
+
+
+
+
+
+
+
+
